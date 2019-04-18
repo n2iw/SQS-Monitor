a698750f4c7504734aa4bdc4598ed84f8c154481
Ming Ying <yingming18@gmail.com>
2019-04-18 11:07:25 -0400

    Dynamically get queue names and message counts.

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, send_file, send_from_directory, jsonify
+from flask import Flask, send_file, send_from_directory, jsonify, request, abort
+from sqs import *
 
 app = Flask(__name__)
 
+# Return list of dicts like
+# {
+#   'tie': 'dev'
+#   'name': 'pimixture.fifo'
+# }
+#
+def getQueueNames(product):
+    tiers = ['dev', 'qa']
+    queue_names = {
+        'pimixture.dev': 'pimixture.fifo',
+        'pimixture.qa': 'pimixture2.fifo',
+        'microarray.dev': 'microarray.fifo'
+    }
+
+    names = []
+
+    for tier in tiers:
+        key = '{}.{}'.format(product.lower(), tier.lower())
+        if key in queue_names:
+            names.append({'tier': tier, 'name': queue_names[key]})
+    return names
 
 @app.route('/')
 def main():
@@ -18,7 +40,19 @@ def send_css(path):
 @app.route('/queryStatus')
 def queryStatus():
     print('Query Status')
-    return jsonify({'avl': 50, 'inflt': 10 })
+    product = request.args.get('product')
+    if product:
+        results = []
+        print('product: {}'.format(product))
+        queue_names = getQueueNames(product)
+        for queue in queue_names:
+            q = Queue(queue['name'])
+            avl = q.getNumberOfAvailableMessages()
+            inflight = q.getNumberOfInFlightMessages()
+            results.append({'tier': queue['tier'], 'queue_name': queue['name'], 'avl': avl, 'inflt': inflight})
+        return jsonify(results)
+    else:
+        return abort(400)
 
 
 if __name__ == '__main__':
--- js/sqsMonitor.js
+++ js/sqsMonitor.js
@@ -1,28 +1,29 @@
+function queryStatus() {
+    const product = $('#product').val();
+    $.ajax({url: 'queryStatus',
+            data: {product: product}
+    }).done(function(results){
+        console.log(results);
+        for (const queue of results) {
+            const row = $(`#${queue.tier}-row`);
+            row.children('.queue-name').html(queue.queue_name);
+            row.children('.enqueued-messages').html(queue.avl);
+            row.children('.inflight-messages').html(queue.inflt);
+        }
+    });
+}
 
 $(window).on('load', function(){
-    function queryStatus() {
-        const product = $('#product').val();
-        const tier = $('#tier').val();
-        $('#avl-msg').text('');
-        $('#in-flt').text('');
-        $.ajax('queryStatus').done(function(result){
-            console.log(result);
-            $('#avl-msg').text(result.avl);
-            $('#in-flt').text(result.inflt);
-        });
-    }
-
-    console.log("Page loaded!");
-
     $('#product').change(function(e){
         const product = $(e.target).val();
         console.log(product);
         queryStatus();
     });
 
-    $('#tier').change(function(e){
-        const tier = $(e.target).val();
-        console.log(tier)
+    $('#refresh').click(function(e){
+        e.preventDefault();
         queryStatus();
     });
+
+    queryStatus();
 });
